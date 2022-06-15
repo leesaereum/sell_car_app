e8fc2edc9ef286c2310de784bd9d88ba8b4ad251
Rumy <52562826+leesaereum@users.noreply.github.com>
2022-06-15 10:50:27 +0900

    commit

--- lib/pages/community.dart
+++ lib/pages/community.dart
@@ -11,10 +11,6 @@ class _CommunityState extends State<Community> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SELL CAR'),
-        backgroundColor: Color.fromARGB(255, 4, 31, 56),
-      ),
       body: Center(
         child: Column(
           children: [
--- lib/tab.dart
+++ lib/tab.dart
@@ -3,6 +3,10 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:sell_car_app/main.dart';
 import 'package:sell_car_app/pages/community.dart';
+import 'package:sell_car_app/pages/mylist.dart';
+import 'package:sell_car_app/pages/mypage.dart';
+import 'package:sell_car_app/pages/tips.dart';
+import 'package:sell_car_app/static.dart';
 
 class Tabs extends StatefulWidget {
   const Tabs({Key? key}) : super(key: key);
@@ -29,6 +33,10 @@ class _TabsState extends State<Tabs> with SingleTickerProviderStateMixin {
 
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('SELL CAR'),
+        backgroundColor: const Color.fromARGB(255, 4, 31, 56),
+      ),
       body: TabBarView(
         controller: tabcontroller,
         children: const [Home(), Community()],
@@ -49,6 +57,92 @@ class _TabsState extends State<Tabs> with SingleTickerProviderStateMixin {
             ),
           ),
         ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            Container(
+              color: Color.fromARGB(255, 2, 26, 48),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Text(
+                    'Hello [${Static.nickname}]!',
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  ),
+                ],
+              ),
+              height: 160,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ListTile(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const Mypage(),
+                      ));
+                },
+                leading: const CircleAvatar(
+                  backgroundColor: Color.fromARGB(255, 4, 31, 56),
+                  child: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                ),
+                title: const Text('MYPAGE'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+              child: ListTile(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const Mylist(),
+                      ));
+                },
+                leading: const CircleAvatar(
+                  backgroundColor: Color.fromARGB(255, 4, 31, 56),
+                  child: Icon(
+                    Icons.list,
+                    color: Colors.white,
+                  ),
+                ),
+                title: const Text('MYLIST'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ListTile(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const Tips(),
+                      ));
+                },
+                leading: const CircleAvatar(
+                  backgroundColor: Color.fromARGB(255, 4, 31, 56),
+                  child: Icon(
+                    Icons.lightbulb,
+                    color: Colors.white,
+                  ),
+                ),
+                title: const Text('TIPS'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
