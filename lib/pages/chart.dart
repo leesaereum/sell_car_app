0702289044eb03ecb2be02cf2c0ed375df7a3c45
thomyorker <thomyorker7@gmail.com>
2022-06-16 21:17:26 +0900

    [update] 22/06/16 charts in process

--- lib/pages/chart.dart
+++ lib/pages/chart.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
 import "package:charts_flutter/flutter.dart" as charts;
 import 'package:flutter/material.dart';
 import 'package:sell_car_app/chart_deveoloper.dart';
 import 'package:sell_car_app/chart_series.dart';
+import 'package:sell_car_app/static.dart';
+import 'package:http/http.dart' as http;
 
 class Chart extends StatefulWidget {
   final int inputAge;
@@ -26,38 +29,120 @@ class Chart extends StatefulWidget {
 }
 
 class _ChartState extends State<Chart> {
-  final List<DeveloperSeries> data = [];
-  final dynamic price = 1000; 
+  final dynamic price = 1000; // 임시로 만든 것임
+  final String model = Static.model; // 모델명
+  final _valueList = ['Age', 'FuelType', 'Transmission']; // DropDownButton List
+  String _selectedValue = 'Age'; // DrowDown 선택값
+  final List jsonData = []; // db jsondata 리스트
+  String jspUrl = ""; // db 가져오는 jsp url 주소
+  final List<DeveloperSeries> data = []; // 차트 그릴 때 쓰는 data 리스트
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    data.add(DeveloperSeries(
-        feature: widget.inputAge, 
-        target: price, 
-        chartColor: charts.ColorUtil.fromDartColor(Colors.green),),);
 
-    data.add(DeveloperSeries(
-        feature: widget.inputAge+5, 
-        target: price, 
-        chartColor: charts.ColorUtil.fromDartColor(Colors.red),),);
+    // 차트 그리는 data 리스트 채우기
+    data.add(
+      DeveloperSeries(
+        feature: widget.inputAge,
+        target: price,
+        chartColor: charts.ColorUtil.fromDartColor(Colors.green),
+      ),
+    );
 
-    data.add(DeveloperSeries(
-        feature: widget.inputAge+10, 
-        target: price, 
-        chartColor: charts.ColorUtil.fromDartColor(Colors.green),),);
+    data.add(
+      DeveloperSeries(
+        feature: widget.inputAge + 5,
+        target: price,
+        chartColor: charts.ColorUtil.fromDartColor(Colors.red),
+      ),
+    );
+
+    data.add(
+      DeveloperSeries(
+        feature: widget.inputAge + 10,
+        target: price,
+        chartColor: charts.ColorUtil.fromDartColor(Colors.green),
+      ),
+    );
   }
 
+// 화면 그림
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Bar Chart"),
-      ),
+          backgroundColor: const Color.fromARGB(255, 4, 31, 56),
+          title: Image.asset(
+            "images/sellcar.png",
+            width: 180,
+          )),
       body: Center(
-        child: DeveloperChart(data: data),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+
+            // Dropdown Button
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("CHOOSE CHART : "),
+                const SizedBox(
+                  width: 30,
+                ),
+                DropdownButton(
+                  value: _selectedValue,
+                  items: _valueList.map(
+                    (value) {
+                      return DropdownMenuItem(
+                        value: value,
+                        child: Text(value),
+                      );
+                    },
+                  ).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedValue = value as String;
+
+                      getJsonData(_selectedValue);
+                    });
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            // Chart
+            jsonData.isEmpty
+                ? const Text("NO DATA")
+                : DeveloperChart(data: data),
+          ],
+        ),
       ),
     );
   }
-}
+
+// Functions
+  Future<bool> getJsonData(String selectedValue) async {
+    if (selectedValue == 'Age') {
+      jspUrl = 'http://localhost:8080/Flutter/query_age_price.jsp?model=$model';
+    } else if (selectedValue == 'Transmission') {
+      jspUrl = 'http://localhost:8080/Flutter/query_transmission_price.jsp?model=$model';
+    } else if (selectedValue == 'FuelType') {
+      jspUrl = 'http://localhost:8080/Flutter/query_fueltype_price.jsp?model=$model';
+    }
+    var url = Uri.parse(jspUrl);
+    var response = await http.get(url);
+
+    setState(() {
+      var dataConvertedJSON = json.decode(utf8.decode(response.bodyBytes));
+      // - 키 값 이용해서 안쪽 데이터에 접근
+      List result = dataConvertedJSON['results'];
+      jsonData.addAll(result);
+    });
+    return true;
+  }
+} // End
--- lib/pages/checkbrand.dart
+++ lib/pages/checkbrand.dart
@@ -25,102 +25,107 @@ class _CheckBrandState extends State<CheckBrand> {
             GestureDetector(
               onTap: () {
                 setState(() {
-                Static.brand = 'Audi';
+                  Static.brand = 'Audi';
                 });
               },
               child: Container(
                 decoration: BoxDecoration(
-                  border: Border.all(
-                    color:Static.brand == "Audi"?Colors.amber:Color.fromARGB(255, 4, 31, 56),
-                    width: 3,
-                    )
-                ),
+                    border: Border.all(
+                  color: Static.brand == "Audi"
+                      ? Colors.amber
+                      : Color.fromARGB(255, 4, 31, 56),
+                  width: 3,
+                )),
                 width: 350,
                 height: 80,
-                child: 
-                  Image.asset('images/audiLogo.png'),
-              ),
-            ),const SizedBox(height: 10),
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                Static.brand = 'Ford';
-                });
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color:Static.brand == "Ford"?Colors.amber:Color.fromARGB(255, 4, 31, 56),
-                    width: 3,
-                    )
-                ),
-                width: 350,
-                height: 80,
-                child: 
-                  Image.asset('images/fordLogo.png'),
-              ),
-            ),const SizedBox(height: 10),
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                Static.brand = 'Vw';
-                });
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color:Static.brand == "Vw"?Colors.amber:Color.fromARGB(255, 4, 31, 56),
-                    width: 3,
-                    )
-                ),
-                width: 350,
-                height: 80,
-                child: 
-                  Image.asset('images/volkswagenLogo.png'),
-              ),
-            ),const SizedBox(height: 10),
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                Static.brand = 'Benz';
-                });
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color:Static.brand == "Benz"?Colors.amber:Color.fromARGB(255, 4, 31, 56),
-                    width: 3,
-                    )
-                ),
-                width: 350,
-                height: 80,
-                child: 
-                  Image.asset('images/benzLogo.png'),
-              ),
-            ),const SizedBox(height: 10),
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                Static.brand = 'Bmw';
-                });
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color:Static.brand == "Bmw"?Colors.amber:Color.fromARGB(255, 4, 31, 56),
-                    width: 3,
-                    )
-                ),
-                width: 350,
-                height: 80,
-                child: 
-                  Image.asset('images/bmwLogo.png'),
+                child: Image.asset('images/audiLogo.png'),
               ),
             ),
-            const SizedBox(height:30),
+            const SizedBox(height: 10),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  Static.brand = 'Ford';
+                });
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(
+                  color: Static.brand == "Ford"
+                      ? Colors.amber
+                      : Color.fromARGB(255, 4, 31, 56),
+                  width: 3,
+                )),
+                width: 350,
+                height: 80,
+                child: Image.asset('images/fordLogo.png'),
+              ),
+            ),
+            const SizedBox(height: 10),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  Static.brand = 'Vw';
+                });
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(
+                  color: Static.brand == "Vw"
+                      ? Colors.amber
+                      : Color.fromARGB(255, 4, 31, 56),
+                  width: 3,
+                )),
+                width: 350,
+                height: 80,
+                child: Image.asset('images/volkswagenLogo.png'),
+              ),
+            ),
+            const SizedBox(height: 10),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  Static.brand = 'Benz';
+                });
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(
+                  color: Static.brand == "Benz"
+                      ? Colors.amber
+                      : Color.fromARGB(255, 4, 31, 56),
+                  width: 3,
+                )),
+                width: 350,
+                height: 80,
+                child: Image.asset('images/benzLogo.png'),
+              ),
+            ),
+            const SizedBox(height: 10),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  Static.brand = 'Bmw';
+                });
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(
+                  color: Static.brand == "Bmw"
+                      ? Colors.amber
+                      : Color.fromARGB(255, 4, 31, 56),
+                  width: 3,
+                )),
+                width: 350,
+                height: 80,
+                child: Image.asset('images/bmwLogo.png'),
+              ),
+            ),
+            const SizedBox(height: 30),
             PrimaryButton(
-              onPressed : widget.onNext,
-              text:'NEXT',),
+              onPressed: widget.onNext,
+              text: 'NEXT',
+            ),
           ],
         ),
       ),
@@ -139,9 +144,8 @@ class PrimaryButton extends StatelessWidget {
       height: 48,
       width: 100,
       child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          primary: Color.fromARGB(255, 4, 31, 56)
-        ),
+        style:
+            ElevatedButton.styleFrom(primary: Color.fromARGB(255, 4, 31, 56)),
         onPressed: onPressed,
         child: Text(
           text,
