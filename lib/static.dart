05b9ad6245396fc9ac83f49c9209340b8c4fb25f
thomyorker <thomyorker7@gmail.com>
2022-06-17 16:09:18 +0900

    conflicts resolved

--- lib/static.dart
+++ lib/static.dart
@@ -1,10 +1,6 @@
 class Static {
   static String id = "test";
-<<<<<<< HEAD
-  static String nickname = "Guest";
-=======
   static String nickname = "Guest"; 
   static String brand = "";
   static String model = "";
->>>>>>> jinyoung
 }
