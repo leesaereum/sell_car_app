cb0ddd13dc423ed6c99d66b72da09fdcecf83148
naddda0813 <jiyeah00813@gmail.com>
2022-07-01 18:25:06 +0900

    commit

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,11 +1,10 @@
 import 'dart:convert';
 
-import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+//import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
 import 'package:sell_car_app/pages/check.dart';
-import 'package:sell_car_app/pages/checkbrand.dart';
 import 'package:sell_car_app/pages/signin.dart';
 import 'package:sell_car_app/provider/google_sign_in.dart';
 import 'package:sell_car_app/static.dart';
@@ -18,26 +17,8 @@ class Login extends StatefulWidget {
   State<Login> createState() => _LoginState();
 }
 
-class _LoginState extends State<Login> {
-  Future<UserCredential> signInWithGoogle() async {
-  // Trigger the authentication flow
-  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-  // Obtain the auth details from the request
-  final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
-
-  // Create a new credential
-  final credential = GoogleAuthProvider.credential(
-    accessToken: googleAuth?.accessToken,
-    idToken: googleAuth?.idToken,
-  );
-
-  // Once signed in, return the UserCredential
-  return await FirebaseAuth.instance.signInWithCredential(credential);
-}
-
-
-  //password
+class _LoginState extends State<Login> { 
+//password
   // 비번 visible= 안보임이 초기상태
   bool _pwState = true;
   String Hint = "";
@@ -45,13 +26,30 @@ class _LoginState extends State<Login> {
   TextEditingController pwController = TextEditingController();
   //유효성 검사
   GlobalKey<FormState> formkey = GlobalKey<FormState>();
+// Future<UserCredential> signInWithGoogle() async {
+  // // Trigger the authentication flow
+  // final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-  @override
+  // // Obtain the auth details from the request
+  // final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+  // // Create a new credential
+  // final credential = GoogleAuthProvider.credential(
+  //   accessToken: googleAuth?.accessToken,
+  //   idToken: googleAuth?.idToken,
+  // );
+
+  // // Once signed in, return the UserCredential
+  // return await FirebaseAuth.instance.signInWithCredential(credential);
+
+@override
   void initState() {
     super.initState();
     _pwState = true;
   }
 
+
+  
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -301,10 +299,10 @@ class _LoginState extends State<Login> {
                   ),
                   ElevatedButton.icon(
                       onPressed: () {
-                        final provider = Provider.of<GoogleSignInProvider>(
-                            context,
-                            listen: false);
-                        provider.googleLogin();
+                        // final provider = Provider.of<GoogleSignInProvider>(
+                        //     context,
+                        //     listen: false);
+                        // provider.googleLogin();
                       },
                       icon: Image.asset(
                         "images/googlelogo.png",
@@ -385,5 +383,5 @@ class _LoginState extends State<Login> {
       });
     }
   }
-} 
-//END
+
+}//END
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/tj/Documents/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Volumes/Data/KJinyoung/Github/sell_car_app"
+export "FLUTTER_ROOT=/Users/tj/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/tj/Documents/GitHub/sell_car_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
