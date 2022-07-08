c56e5a56266b0207869f8c2c2bcb1fcd273e40c3
Rumy <52562826+leesaereum@users.noreply.github.com>
2022-07-08 17:11:06 +0900

    commit

--- lib/static.dart
+++ lib/static.dart
@@ -1,13 +1,7 @@
 class Static {
-<<<<<<< HEAD
   static String id = "";
   static String pw = "";
   static String nickname = "";
-=======
-  static String id = "jinyoung";
-  static String pw = "";
-  static String nickname = "zzangjin";
->>>>>>> jinyoung
   static String signinday = "";
   static String leaveday = "";
   static String brand = "";
@@ -20,6 +14,6 @@ class Static {
   static int pnum = 1;
   static String comment = "";
   static int cnum = 1;
-  static String cnickname="";
-  static String ccreateAt="";
+  static String cnickname = "";
+  static String ccreateAt = "";
 }
